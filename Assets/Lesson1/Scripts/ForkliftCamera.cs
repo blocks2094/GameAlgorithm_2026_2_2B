fa0525815dcf0f1c91c7eed67ca5e5ba1038b73c
HyunWoo <blocks2094@g.shingu.ac.kr>
2026-09-08 14:54:24 +0900

    [Feat] Queue, Stack 예시 구현

--- Assets/Lesson1/Scripts/ForkliftCamera.cs
+++ Assets/Lesson1/Scripts/ForkliftCamera.cs
@@ -4,7 +4,7 @@ public sealed class ForkliftCamera : MonoBehaviour
 {
     // 제공 코드: 지게차의 로컬 뒤쪽 위치를 부드럽게 따라갑니다.
     public Transform target;
-    public Vector3 offset=new Vector3(0,6,-7);
+    public Vector3 offset=new Vector3(0,15,-7);
     void LateUpdate()
     {
         if (target == null)
